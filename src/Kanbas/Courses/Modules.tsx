c1986387f9ff630e4cb26f6b0135bb27b61011e3
Yash Beladiya <yashhbeladiya@gmail.com>
2024-09-18 16:50:30 -0400

    Home Screen in Courses created

--- src/Kanbas/Courses/Modules.tsx
+++ src/Kanbas/Courses/Modules.tsx
@@ -2,10 +2,14 @@ export default function Modules() {
     return (
       <div>
         {/* Implement Collapse All button, View Progress button, etc. */}
-        <button id="wd-button-collapse-all">Collapse All</button> 
-        <button id="wd-button-view-progess">View Progress</button> 
-        <button id="wd-button-publish-all">Publish All</button> 
-        <button id="wd-button-module">+ Module</button>
+        <div id="wd-modules-buttons">
+            <button id="wd-button-collapse-all">Collapse All</button> <button id="wd-button-view-progess">View Progress</button> <select id="wd-select-publish">
+                <option value="publish-all">Publish All</option>
+                <option value="week1">Week1</option>
+                <option value="week2">Week2</option>
+                <option value="week3">Week3</option>
+            </select> <button id="wd-button-module">+ Module</button>
+        </div>
 
         <ul id="wd-modules">
           <li className="wd-module">
@@ -27,6 +31,7 @@ export default function Modules() {
                 <span className="wd-title">SLIDES</span>
                     <li className="wd-content-item">Introduction to Web Development</li>
                     <li className="wd-content-item">Creating an HTTP server with Node.js</li>
+                    <li className="wd-content-item">Creating a React Application</li> 
               </li>
             </ul>
           </li>
@@ -37,7 +42,12 @@ export default function Modules() {
                 <span className="wd-title">LEARNING OBJECTIVES</span>
                 <ul className="wd-content">
                     <li className="wd-content-item">Learn how to create user interfaces with HTML</li>
-                    <li className="wd-content-item">Keep working on assignment 1</li>
+                    <li className="wd-content-item">Deploy the assignment to Netlify</li>
+                </ul>
+                <span className="wd-title">SLIDES</span>
+                <ul className="wd-content">
+                    <li className="wd-content-item">Embedding content with Iframes</li>
+                    <li className="wd-content-item">Drawing with Scalable Vector Graphics</li>
                 </ul>
               </li>
             </ul>
--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import CoursesNavigation from "./Navigation";
 import Modules from "./Modules";
+import Home from "./Home";
 
 export default function Courses() {
     return(
@@ -15,7 +16,7 @@ export default function Courses() {
                 <td valign="top">
                     <Routes>
                     <Route path="/" element={<Navigate to="Home" />} />
-                    <Route path="Home" element={<h2>Home</h2>} />
+                    <Route path="Home" element={< Home />} />
                     <Route path="Modules" element={< Modules />} />
                     <Route path="Assignments" element={<h2>Assignments</h2>} />
                     <Route path="Assignments/:aid" element={<h2>Assignment Editor</h2>} />
