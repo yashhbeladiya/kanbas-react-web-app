9c4b8c502fba922bc6918a479e96494d8296c27b
Yash Beladiya <yashhbeladiya@gmail.com>
2024-10-09 17:45:01 -0400

    Started with lab3

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,7 +1,20 @@
+import BooleanVariables from "./BooleanVariables";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInline";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
+import VariablesAndConstants from "./VariablesAndConstants";
+
 export default function Lab3() {
     return (
         <div>
             <h2>Lab 3</h2>
+            <VariablesAndConstants />
+            <BooleanVariables />
+            <IfElse />
+            <TernaryOperator />
+            <ConditionalOutputIfElse />
+            <ConditionalOutputInline />
         </div>
     );
 }
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -5,7 +5,7 @@ import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
 export default function Labs() {
   return (
-    <div id="wd-labs">
+    <div id="wd-labs" className="m-2">
       <h1>Labs</h1>
         <TOC />
         <Routes>
